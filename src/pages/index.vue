84de29e1807db3ac0c9ab8e07041d92244f567d2
RoySung <roysunghan@gmail.com>
2022-05-17 08:25:32 +0800

    Initialize feature/withoutDragonBones

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { createGame, MainScene } from '~/game'
+
+const game = ref()
+const getMainScene = () => game.value.scene.getScene('Game') as MainScene
+const watch = async() => {
+}
+
+onMounted(() => {
+  game.value = createGame()
+})
+
+</script>
+
+<template>
+  <div id="flappy-bird-game" class="flex justify-center self-center">
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
