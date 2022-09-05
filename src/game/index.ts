ab37c2ebfd78983efbaab7a72d8b3707cc2218ba
RoySung <roysunghan@gmail.com>
2022-09-05 16:01:36 +0800

    feat: 🎸 handle bg and add main role with animations(idle, walk)

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,21 +1,140 @@
 import Phaser from 'phaser'
-import logoImg from '/logo.png'
+import poringIdleSprite from '/sprites/poring/poring-idle-sprite.png'
+import poringWalkSprite from '/sprites/poring/poring-walk-sprite.png'
+import bg0Img from '/bg-0.png'
+
+enum PoringRole {
+  Idle = 'poring-idle',
+  Walk = 'poring-walk'
+}
 
 export class MainScene extends Phaser.Scene {
+  bg?: Phaser.GameObjects.TileSprite
+  mainRole?: Phaser.GameObjects.Sprite
+  mainRoleMap: Map<PoringRole, Phaser.GameObjects.Sprite>
+  mainRoleStartPosition = {
+    x: 0,
+    y: 0,
+  }
+
   constructor(config: string | Phaser.Types.Scenes.SettingsConfig) {
     super(config)
+
+    this.mainRoleMap = new Map()
   }
 
   preload() {
-    this.load.image('logo', logoImg)
+    this.preloadBackground()
+    this.preloadMainCharacter()
+  }
+
+  preloadBackground() {
+    this.load.image('bg-0', bg0Img)
+  }
+
+  preloadMainCharacter() {
+    const { width, height } = this.game.canvas
+    const x = width / 2
+    const y = height / 2 + 300
+    this.mainRoleStartPosition = { x, y }
+
+    this.load.spritesheet(PoringRole.Idle, poringIdleSprite, { frameWidth: 41, frameHeight: 39 })
+    this.load.spritesheet(PoringRole.Walk, poringWalkSprite, { frameWidth: 41, frameHeight: 44 })
   }
 
   create() {
-    const x = this.game.canvas.width / 2
-    const y = this.game.canvas.height / 2
-    this.add.image(x, y, 'logo')
+    this.createBackground()
+    this.createMainCharacter()
+    this.playMainRole(PoringRole.Idle)
 
-    // this.add.text(0, 0, 'Hello World!')
+    setTimeout(() => {
+      this.doWalking()
+    }, 2000)
+  }
+
+  createBackground() {
+    const { width, height } = this.game.canvas
+    this.add.rectangle(0, 0, width, height, 0x6B7FAF).setOrigin(0, 0)
+    const tileWHRate = 1228 / 800
+    const tileWidth = width * tileWHRate
+    this.bg = this.add.tileSprite(0, 0, tileWidth, height, 'bg-0')
+    this.bg.setOrigin(0, 0)
+  }
+
+  createMainCharacter() {
+    const roleCreators = [
+      () => this.createIdleMainRole(),
+      () => this.createWalkMainRole(),
+    ]
+    roleCreators.forEach(creator => creator())
+  }
+
+  createIdleMainRole() {
+    const role = this.mainRoleMap.get(PoringRole.Idle)
+    if (role) return role
+
+    const { x, y } = this.mainRoleStartPosition
+    this.anims.create({
+      key: PoringRole.Idle,
+      frames: this.anims.generateFrameNumbers(PoringRole.Idle, { start: 0, end: 4 }),
+      frameRate: 8,
+      repeat: -1,
+    })
+    const poring = this.add.sprite(x, y, PoringRole.Idle)
+    poring.setFlipX(true)
+    poring.setScale(2)
+    poring.setVisible(false)
+    this.mainRoleMap.set(PoringRole.Idle, poring)
+
+    return poring
+  }
+
+  createWalkMainRole() {
+    const role = this.mainRoleMap.get(PoringRole.Walk)
+    if (role) return role
+
+    const { x, y } = this.mainRoleStartPosition
+    this.anims.create({
+      key: PoringRole.Walk,
+      frames: this.anims.generateFrameNumbers(PoringRole.Walk, { start: 0, end: 9 }),
+      frameRate: 26,
+      repeat: -1,
+    })
+    const poring = this.add.sprite(x, y, PoringRole.Walk)
+    poring.setFlipX(true)
+    poring.setScale(2)
+    poring.setVisible(false)
+    this.mainRoleMap.set(PoringRole.Walk, poring)
+
+    return poring
+  }
+
+  playMainRole(role: PoringRole) {
+    const targetRole = this.mainRoleMap.get(role)
+    if (!targetRole) return
+
+    if (this.mainRole) {
+      const { x, y } = this.mainRole
+      targetRole.setPosition(x, y)
+      this.mainRole.setVisible(false)
+    }
+
+    targetRole.setVisible(true)
+    this.mainRole = targetRole
+    this.mainRole.play(role)
+  }
+
+  doWalking() {
+    this.playMainRole(PoringRole.Walk)
+  }
+
+  get isWalking() {
+    const walkRole = this.mainRoleMap.get(PoringRole.Walk)
+    return this.mainRole === walkRole
+  }
+
+  update() {
+    if (this.bg && this.isWalking) this.bg.tilePositionX += 3
   }
 }
 
@@ -26,14 +145,15 @@ const config = {
   height: 800,
 }
 
-export const createGame = () => {
-  class Game extends Phaser.Game {
-    constructor(GameConfig?: Phaser.Types.Core.GameConfig) {
-      super(GameConfig)
-      this.scene.add('Game', MainScene)
-      this.scene.start('Game')
-    }
+class Game extends Phaser.Game {
+  constructor(GameConfig?: Phaser.Types.Core.GameConfig) {
+    super(GameConfig)
+    this.scene.add('Game', MainScene)
+    this.scene.start('Game')
   }
+}
+
+export const createGame = () => {
   const game = new Game(config)
   return game
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { Ref } from 'vue'
 import ReadyStage from '~/components/gameUIStages/ReadyStage.vue'
 import FocusStage from '~/components/gameUIStages/FocusStage.vue'
 import RestStage from '~/components/gameUIStages/RestStage.vue'
 import { createGame, MainScene } from '~/game'
 import { useCountdownModel } from '~/models/countdownModel'
 
-const game = ref()
+const game = ref() as Ref<ReturnType<typeof createGame>>
 const getMainScene = () => game.value.scene.getScene('Game') as MainScene
 const watch = async() => {
 }
@@ -27,6 +28,10 @@ onMounted(() => {
   game.value = createGame()
 })
 
+const doWalking = () => {
+
+}
+
 </script>
 
 <template>
@@ -34,6 +39,9 @@ onMounted(() => {
     <div id="trip-pomodoro-canvas-wrap">
     </div>
     <div class="stage-wrap absolute top-0 left-0 mr-auto w-full h-full">
+      <button @click="doWalking">
+        DoWalk
+      </button>
       <component :is="stageComponent"></component>
     </div>
   </div>
