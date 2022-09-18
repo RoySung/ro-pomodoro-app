7307444696ad7392eb4008b6191f1f246fd1cc85
RoySung <roysunghan@gmail.com>
2022-09-18 15:30:39 +0800

    feat: 🎸 change favicon

--- vite.config.ts
+++ vite.config.ts
@@ -98,24 +98,24 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+      includeAssets: ['robots.txt'],
       manifest: {
         name: 'Vitesse',
         short_name: 'Vitesse',
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/favicon/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/favicon/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/favicon/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
