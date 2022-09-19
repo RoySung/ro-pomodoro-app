e11535eaf976bbc5ab61087808592785c030a8ee
RoySung <roysunghan@gmail.com>
2022-09-19 15:29:00 +0800

    feat: 🎸 add meta tag

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
+import poring from '/game/poring/poring-walk.gif'
 
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
@@ -7,7 +8,26 @@ import { useHead } from '@vueuse/head'
 useHead({
   title: 'Ro Pomodoro App',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    {
+      property: 'og:title',
+      content: 'Ro Pomodoro App',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:image',
+      content: poring,
+    },
+    {
+      property: 'og:description',
+      content: 'Simple Pomodoro App',
+    },
+    {
+      name: 'description',
+      content: 'Simple Pomodoro App',
+    },
   ],
 })
 </script>
